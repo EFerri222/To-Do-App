e36d114e6a5348d71739b757cbb44326dee7a5cd
EFerri222 <eferri222@gmail.com>
2020-03-17 01:33:12 -0400

    Refactor JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,24 @@
-// Add To-Do item when Enter is pressed on input
-$("input").on("keypress", function(event) {
-    if(event.keyCode === 13 && $(this).val() !== "") {
-        $("ul").append("<li><span class=\"remove\">X</span> " + $(this).val() + "</li>");
-        $(this).val("");
-    }
-});
+$(document).ready(function() {
 
-// Remove to-do item when button is clicked
-$(document).on("click", ".remove", function() {
-    $(this).parent().remove();
-});
+    // Add To-Do item when Enter is pressed on input
+    $("input").on("keypress", function(event) {
+        if(event.keyCode === 13 && $(this).val() !== "") {
+            $("ul").append("<li><span>X</span> " + $(this).val() + "</li>");
+            $(this).val("");
+        }
+    });
 
-// Toggle completed when to-do item is clicked 
-$(document).on("click", "li", function() {
-    $(this).toggleClass("completed");
+    // Remove to-do item when X is clicked
+    $("ul").on("click", "span", function(event) {
+        event.stopPropagation();
+        $(this).parent().fadeOut(function() {
+            $(this).remove();
+        });
+    });
+
+    // Toggle completed when to-do item is clicked 
+    $("ul").on("click", "li", function() {
+        $(this).toggleClass("completed");
+    });
+
 });
