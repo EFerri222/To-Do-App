3bcd989544f84f9879ce7c42277039fdb4223fa6
EFerri222 <eferri222@gmail.com>
2020-03-16 21:20:20 -0400

    Add functionality for listeners to affect dynamically created to-dos

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,17 @@
+// Add To-Do item when Enter is pressed on input
+$("input").on("keypress", function(event) {
+    if(event.keyCode === 13 && $(this).val() !== "") {
+        $("ul").append("<li><span class=\"remove\">X</span> " + $(this).val() + "</li>");
+        $(this).val("");
+    }
+});
+
 // Remove to-do item when button is clicked
-$("button").on("click", function() {
+$(document).on("click", ".remove", function() {
     $(this).parent().remove();
 });
 
 // Toggle completed when to-do item is clicked 
-$("li").on("click", function() {
+$(document).on("click", "li", function() {
     $(this).toggleClass("completed");
-});
-
-// Add To-Do item when Enter is pressed on input
-$("input").on("keypress", function(event) {
-    if(event.keyCode === 13 && $(this).val() !== "") {
-        $("ul").append("<li class=\"todo-item\"><button>Delete</button> " + $(this).val() + "</li>");
-        $(this).val("");
-    }
 });
